f5714ce31408d5fcc08649c7b731c78e84522e67
rkpoint1 <45294617+rkpoint1@users.noreply.github.com>
2018-11-24 02:32:24 +0600

    CSS

--- wp-login.php
+++ wp-login.php
@@ -9,7 +9,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Log In &lsaquo; Albi Fashion &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' href='/https://rkpoint1.github.io/albifashion/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.8' type='text/css' media='all' />
+<link rel='stylesheet' href='http://localhost:8080/https:/rkpoint1.github.io/albifashion/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.8' type='text/css' media='all' />
 <meta name='robots' content='noindex,follow' />
 	<meta name="viewport" content="width=device-width" />
 		</head>
@@ -17,7 +17,7 @@
 		<div id="login">
 		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Powered by WordPress</a></h1>
 	
-<form name="loginform" id="loginform" action="/https://rkpoint1.github.io/albifashion/wp-login.php" method="post">
+<form name="loginform" id="loginform" action="http://localhost:8080/https:/rkpoint1.github.io/albifashion/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Username or Email Address<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
@@ -35,7 +35,7 @@
 </form>
 
 <p id="nav">
-	<a href="/https://rkpoint1.github.io/albifashion/wp-login.php?action=lostpassword">Lost your password?</a>
+	<a href="http://localhost:8080/https:/rkpoint1.github.io/albifashion/wp-login.php?action=lostpassword">Lost your password?</a>
 </p>
 
 <script type="text/javascript">
@@ -52,7 +52,7 @@ wp_attempt_focus();
 if(typeof wpOnload=='function')wpOnload();
 </script>
 
-	<p id="backtoblog"><a href="/https://rkpoint1.github.io/albifashion/">&larr; Back to Albi Fashion</a></p>
+	<p id="backtoblog"><a href="http://localhost:8080/https:/rkpoint1.github.io/albifashion/">&larr; Back to Albi Fashion</a></p>
 		
 	</div>
 
